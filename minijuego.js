4747e4fc03c2ba00a035e7d64563787b6e3c7f5a
unknown <santicorocottax@gmail.com>
2021-12-20 23:57:28 +0100

    He conseguido completar la página del minijuego, solo falta la funcionalidad del juego

--- minijuego.js
+++ minijuego.js
@@ -1,5 +1,9 @@
 const divPrincipal = document.querySelector(".divPrincipal");
 const urlSuelo = "./imgs/suelo.gif";
+const urlDigglet = "./imgs/digglet.gif";
+const botonJugar = document.querySelector(".botonJugar");
+
+botonJugar.addEventListener("click", () => jugar());
 
 for (let i = 0; i < 9; i++) {
     
@@ -8,8 +12,58 @@ for (let i = 0; i < 9; i++) {
     suelo.setAttribute("src", urlSuelo);
     suelo.classList.add("suelos");
 
+    if (suelo.className === "digglet") {
+        
+        
+
+    }
+
     divPrincipal.appendChild(suelo);
     
+}
+
+const jugar = () => {
+
+    const suelos = document.querySelectorAll(".suelos");
+
+    let timeOut = 1000;
+
+    let anterior = 0;
+
+    for (let i = 29; i >= 0; i--) {
+
+        let random = Math.floor(Math.random()*9);
+
+        setTimeout(() => {
+
+            const cambiarContador = document.querySelector(".contador");
+
+            suelos[anterior].setAttribute("src", urlSuelo);
+
+            suelos[random].setAttribute("src", urlDigglet);
+            suelos[random].classList.add("digglet");
+            anterior = random;
+            
+            if (i < 10) {
+
+                cambiarContador.textContent = "00:0" + i;
+                cambiarContador.style = "color: red";
+
+            } else {
+
+                cambiarContador.style = "color: white";
+                cambiarContador.textContent = "00:" + i;
+
+            }
+            
+        }, timeOut);
+
+        timeOut += 1000;
+
+        suelos[anterior].setAttribute("src", urlSuelo);
+        
+    }
+
 }
 
 
