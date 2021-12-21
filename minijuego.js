f87aaf34f1ffb11b2acbe5228f7d18baf7850f6e
unknown <santicorocottax@gmail.com>
2021-12-21 13:05:51 +0100

    casi terminado solo faltan retoques

--- minijuego.js
+++ minijuego.js
@@ -4,19 +4,28 @@ const urlDigglet = "./imgs/digglet.gif";
 const botonJugar = document.querySelector(".botonJugar");
 let puntuacion = 0;
 
-botonJugar.addEventListener("click", () => jugar());
+botonJugar.addEventListener("click", () => pintarJuego());
 
-for (let i = 0; i < 9; i++) {
-    
-    const suelo = document.createElement("img");
+const pintarJuego = () => {
 
-    suelo.addEventListener("click", () => mazazo());
+    divPrincipal.innerHTML = `<div class ="juego"></div>`;
 
-    suelo.setAttribute("src", urlSuelo);
-    suelo.classList.add("suelos");
+    for (let i = 0; i < 9; i++) {
+        
+        const div$$ = document.querySelector(".juego");
+        const suelo = document.createElement("img");
 
-    divPrincipal.appendChild(suelo);
-    
+        suelo.addEventListener("click", () => mazazo(suelo));
+
+        suelo.setAttribute("src", urlSuelo);
+        suelo.classList.add("suelos");
+
+        div$$.appendChild(suelo);
+        
+    }
+
+    jugar();
+
 }
 
 const jugar = () => {
@@ -33,52 +42,74 @@ const jugar = () => {
 
         setTimeout(() => {
 
-            const cambiarContador = document.querySelector(".contador");
+            if (i != 0) {
+                
+                const cambiarContador = document.querySelector(".contador");
 
-            suelos[anterior].setAttribute("src", urlSuelo);
-
-            suelos[random].setAttribute("src", urlDigglet);
-            suelos[random].classList.add("digglet");
-            anterior = random;
-            
-            if (i < 10) {
-
-                cambiarContador.textContent = "00:0" + i;
-                cambiarContador.style = "color: red";
+                suelos[anterior].setAttribute("src", urlSuelo);
+                suelos[anterior].classList.replace("digglet", "suelos");
+    
+                suelos[random].setAttribute("src", urlDigglet);
+                suelos[random].classList.replace("suelos", "digglet");
+                anterior = random;
+                
+                if (i < 10) {
+    
+                    cambiarContador.textContent = "00:0" + i;
+                    cambiarContador.style = "color: red";
+    
+                } else {
+    
+                    cambiarContador.style = "color: white";
+                    cambiarContador.textContent = "00:" + i;
+    
+                }
 
             } else {
 
-                cambiarContador.style = "color: white";
-                cambiarContador.textContent = "00:" + i;
+                const cambiarContador = document.querySelector(".contador");
+                suelos[random].setAttribute("src", urlSuelo);
+                suelos[random].classList.replace("digglet", "suelos");
+                cambiarContador.textContent = "00:0" + i;
+
+                comprobacion();
 
             }
             
         }, timeOut);
 
         timeOut += 1000;
-
-        suelos[anterior].setAttribute("src", urlSuelo);
         
     }
 
 }
 
 
-const mazazo = () => {
+const mazazo = (suelo) => {
 
-    const divPrincipal = document.querySelector(".divPrincipal");
-    const suelos = divPrincipal.querySelectorAll("img");
-
-    for (let i = 0; i < suelos.length; i++) {
-
-            const suelo = suelos[i];
-
-            puntuacion += 5; 
-            console.log(puntuacion);
+    if (suelo.className.includes("digglet")) {
         
-            let actualizarPuntuacion = suelo.querySelector("");
-            actualizarPuntuacion.textContent = ("Puntuación: " + puntuacion);
+        puntuacion += 5; 
+        console.log(puntuacion);
+            
+        let actualizarPuntuacion = document.querySelector(".puntuacion");
+        actualizarPuntuacion.textContent = ("Puntuación: " + puntuacion);
+
+    }   
+
+}
+
+
+const comprobacion = () => {
+
+    if (puntuacion >= 100) {
         
+        divPrincipal.innerHTML = `<div class="resultado"><img src="./imgs/victoria.gif" /><h3>VICTORIA</h3></div>`;
+
+    } else {
+
+        divPrincipal.innerHTML = `<div class="resultado"><img src="./imgs/derrota.gif" /><h3>DERROTA</h3></div>`;
+
     }
 
 }
